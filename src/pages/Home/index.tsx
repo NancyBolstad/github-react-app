bf7544413bb87f1b2f36afdf2c2d6f664f95d1c1
NancyBolstad <litchacha@gmail.com>
2020-04-17 19:47:53 +0200

    Prev and next pagination works now

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Item } from '../../types/data';
 import Typography from '../../components/Typography';
 import MainContent from '../../components/MainContent';
 import { Context } from '../../context/GlobalContext';
@@ -9,7 +10,7 @@ interface Props {}
 
 const Container = styled.table`
   border-collapse: collapse;
-  width: 100%;
+  width: 1080px;
 
   tr:nth-child(even) {
     background-color: #f2f2f2;
@@ -30,7 +31,28 @@ const TableHeader = styled.th`
 
 export const Home: React.FunctionComponent<Props> = () => {
   const localContext = React.useContext(Context);
-  console.log({ localContext });
+  const pageSize = 20;
+  const [totalPage, setTotalPage] = React.useState(0);
+  const [data, setData] = React.useState<Item[]>([]);
+  const [pageNumber, setPageNumber] = React.useState(1);
+
+  function pagination() {
+    const startIndex = (pageNumber - 1) * pageSize;
+    const endIndex = pageSize * pageNumber - 1;
+    const array = localContext.default;
+    const page = array.slice(startIndex, endIndex);
+    console.log({ page });
+    setData([...localContext.default].slice(startIndex, endIndex));
+  }
+
+  React.useEffect(() => {
+    if (localContext) {
+      setData(localContext.default);
+      pagination();
+      setTotalPage(Math.ceil(localContext.default.length / pageSize));
+    }
+  }, [localContext, pageNumber]);
+  console.log({ totalPage });
 
   return (
     <MainContent>
@@ -47,26 +69,48 @@ export const Home: React.FunctionComponent<Props> = () => {
       {localContext.loaded ? (
         <Loader />
       ) : (
-        <Container>
-          <thead>
-            <tr>
-              <TableHeader>Name</TableHeader>
-              <TableHeader>Description</TableHeader>
-              <TableHeader>Forks</TableHeader>
-              <TableHeader>License</TableHeader>
-            </tr>
-          </thead>
-          <tbody>
-          {(localContext.default || []).map((item, index) => (
-            <tr key={index}>
-              <td>{item.name}</td>
-              <td>{item.description}</td>
-              <td>{item.forks}</td>
-              <td>{item.license?.name}</td>
-            </tr>
-          ))}
-          </tbody>
-        </Container>
+        <div>
+          <Container>
+            <thead>
+              <tr>
+                <TableHeader>Name</TableHeader>
+                <TableHeader>Description</TableHeader>
+                <TableHeader>Forks</TableHeader>
+                <TableHeader>License</TableHeader>
+              </tr>
+            </thead>
+            <tbody>
+              {(data || []).map((item, index) => (
+                <tr key={index}>
+                  <td>{item.name}</td>
+                  <td>{item.description}</td>
+                  <td>{item.forks}</td>
+                  <td>{item.license?.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Container>
+          {pageNumber > 1 && (
+            <button
+              onClick={e => {
+                e.preventDefault();
+                setPageNumber(pageNumber - 1);
+              }}
+            >
+              Prev
+            </button>
+          )}
+          {pageNumber < totalPage && (
+            <button
+              onClick={e => {
+                e.preventDefault();
+                setPageNumber(pageNumber + 1);
+              }}
+            >
+              Next
+            </button>
+          )}
+        </div>
       )}
     </MainContent>
   );
