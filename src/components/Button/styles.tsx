c89d8d356ed5e57f5abbc29dce56a8719c221e4a
NancyBolstad <litchacha@gmail.com>
2020-04-18 12:24:35 +0200

    Refactor button styles

--- src/components/Button/styles.tsx
+++ src/components/Button/styles.tsx
@@ -3,37 +3,32 @@ import createFontStyles from '../../util/createFontStyles';
 import { ButtonProps } from '.';
 
 export const Button = styled.button<ButtonProps>`
-  display: flex;
-  width: 100%;
-  justify-content: center;
+  display: inline-flex;
   align-items: center;
+  justify-content: center;
   cursor: pointer;
-  width: fit-content;
   text-decoration: none;
-  border: 2px solid transparent;
-  transition: border-color 0.15s ease-in-out, background-color 0.15s ease-in-out;
+  transition: background-color 0.15s ease-in-out;
+  &:hover,
+  &:active,
+  &:focus {
+    opacity: 0.8;
+  }
   &:disabled {
     pointer-events: none;
-    opacity: 0.6;
+    opacity: 0.5;
   }
   &:focus {
     outline: 0;
   }
-  svg {
-    width: 24px;
-    height: 24px;
-    margin-left: ${props => props.theme.spacing.xs}rem;
-  }
+
   ${props =>
     props.size === 'small' &&
     css`
-      width: 100%;
-      padding: 0 32px;
+      padding: 0.25rem;
+      min-height: 2.75rem;
+      min-width: 2.75rem;
       ${createFontStyles(props.theme.fonts.b2)};
-
-      @media all and (min-width: ${props => props.theme.mediaQueries.small}px) {
-        padding: 0 20px;
-      }
     `};
   ${props =>
     props.size === 'medium' &&
@@ -48,6 +43,7 @@ export const Button = styled.button<ButtonProps>`
       border-radius: 35px;
       ${createFontStyles(props.theme.fonts.h3)};
     `};
+
   ${props =>
     props.variant === 'primary' &&
     css`
@@ -66,53 +62,8 @@ export const Button = styled.button<ButtonProps>`
     props.variant === 'secondary' &&
     css`
       background-color: ${props => props.theme.colors.background};
-      border-color: ${props => props.theme.colors.secondary};
       color: ${props => props.theme.colors.onBackground};
-      svg {
-        fill: ${props => props.theme.colors.secondary};
-      }
-      &:hover,
-      &:active,
-      &:focus {
-        background-color: ${props => props.theme.colors.secondary};
-        color: ${props => props.theme.colors.onSecondary};
-        svg {
-          fill: ${props => props.theme.colors.onSecondary};
-        }
-      }
-    `};
-  ${props =>
-    props.variant === 'tertiary' &&
-    css`
-      background-color: ${props => props.theme.colors.background};
-      color: ${props => props.theme.colors.onBackground};
-      border: none;
-      &:hover,
-      &:active,
-      &:focus {
-        opacity: 0.8;
-      }
-      svg {
-        fill: ${props => props.theme.colors.onBackground};
-      }
-    `};
-  ${props =>
-    props.variant === 'quaternary' &&
-    css`
-      background-color: transparent;
-      color: ${props => props.theme.colors.onPrimary};
-      border-color: ${props => props.theme.colors.onPrimary};
-      &:hover,
-      &:active,
-      &:focus {
-        background-color: ${props => props.theme.colors.onPrimary};
-        color: ${props => props.theme.colors.onBackground};
-        svg {
-          fill: ${props => props.theme.colors.onBackground};
-        }
-      }
-      svg {
-        fill: ${props => props.theme.colors.onPrimary};
-      }
+      border-radius: 1.6em;
+      border: 0;
     `};
 `;
