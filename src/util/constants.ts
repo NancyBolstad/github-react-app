f97e1d0bbf5fcf6efdef601d716f9f29b1cb7a9f
NancyBolstad <litchacha@gmail.com>
2020-04-18 15:28:01 +0200

    Refactor

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -3,7 +3,7 @@ import { Root } from '../types/data';
 export const API_BASE_URL: string =
   'https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=100';
 
-export const PAGE_SIZE = 20;
+export const PAGE_SIZE: number = 20;
 
 export const mockResponse: Root = {
   total_count: 0,
